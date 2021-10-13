cb679b9917cd8a6db657b1810752befeee8475ca
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-12 23:59:17 -0600

    Create tests

--- spec/requests/api/v1/greetings_spec.rb
+++ spec/requests/api/v1/greetings_spec.rb
@@ -8,8 +8,15 @@ RSpec.describe "API::V1::Greetings", type: :request do
     # make HTTP get request before each example
     before { get '/api/v1/random-greeting' }
 
+    it 'returns greeting' do
+      # Note `json` is a custom helper to parse JSON responses
+      expect(json).not_to be_empty
+      expect(json.size).to be >= 4
+    end
+
     it "returns http success" do
-      expect(json).to have_http_status(:success)
+      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
     end
   end
 
