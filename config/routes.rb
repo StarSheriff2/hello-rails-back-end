8b588820adde835352e07bd1a08de5143b83b948
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-19 09:27:03 -0600

    Set up routes and modify controller action names

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :users
+
+      post :sign_in, to: "sessions#create"
+      delete :sign_out, to: "sessions#sign_out"
+      get :logged_in, to: "sessions#logged_in"
       get 'random-greeting', to: 'greetings#hello'
     end
   end
